3dfb93b3b02cf9a6a00e4e590750cc4d9cac9a0e
omarHttb <omar.hattab37@gmail.com>
2022-08-24 12:19:10 +0300

    END OF SECTIO  4

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.data;
 using core.Interfaces;
+using AutoMapper;
+using API.Helpers;
 
 namespace API
 {
@@ -21,6 +23,8 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
@@ -47,6 +51,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
