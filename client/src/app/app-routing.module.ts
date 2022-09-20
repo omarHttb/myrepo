270039cfea031c2286ac5b19cbdee000b0f242b6
omarHttb <omar.hattab37@gmail.com>
2022-09-20 14:33:29 +0300

    end of section 14

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -12,6 +12,13 @@ const routes: Routes = [
   {path: 'not-found',component: NotFoundComponent , data: {breadcrumb: 'not found'} },
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
   data: {breadcrumb: 'shop'}},
+
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule),
+  data: {breadcrumb: 'basket'}},
+  
+  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule),
+  data: {breadcrumb: 'checkout'}},
+
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 
 ];
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
-import { CarouselModule } from 'ngx-bootstrap/carousel'
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { OrderTotalsComponent } from './components/order-totals/order-totals.component'
 
 
 
@@ -11,7 +12,8 @@ import { CarouselModule } from 'ngx-bootstrap/carousel'
 @NgModule({
   declarations: [
     PagingHeaderComponent,
-    PagerComponent
+    PagerComponent,
+    OrderTotalsComponent
   ],
   imports: [
     
@@ -24,7 +26,8 @@ import { CarouselModule } from 'ngx-bootstrap/carousel'
     PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
-    CarouselModule
+    CarouselModule,
+    OrderTotalsComponent
   ]
 })
 export class SharedModule { }
